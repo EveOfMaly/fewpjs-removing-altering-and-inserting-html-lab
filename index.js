a526614da1425618e667389fa9b2e1bae11e013f
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-21 21:37:03 -0400

    commit tree 2fafc1811d7fc2a3db1b13a09f88c49d58402642

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//enter the following code to remove the main node element */
+document.querySelector('main').remove()
+
+/* Create a new element and assign it t newHeader */
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
+document.body.appendChild(newHeader)
